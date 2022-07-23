b793a2fd8fa7913106b5a873ccbcbe415493d24f
Jhericoh Janquill Lumaque <jjnlumaque@iskolarngbayan.pup.edu.ph>
2022-07-23 11:39:00 +0800

    First pass finalization

--- MediktapAdmin-master/Templates/Command.cs
+++ MediktapAdmin-master/Templates/Command.cs
@@ -16,8 +16,8 @@ namespace MediktapAdmin.Templates
 
         public event EventHandler CanExecuteChanged
         {
-            add { CommandManager.RequerySuggested += value; }
-            remove { CommandManager.RequerySuggested -= value; }
+            add => CommandManager.RequerySuggested += value;
+            remove => CommandManager.RequerySuggested -= value;
         }
 
         public bool CanExecute(object parameter)
@@ -29,5 +29,60 @@ namespace MediktapAdmin.Templates
         {
             _action();
         }
+    }
+
+    public class Command<T> : ICommand
+    {
+        private readonly Action<T> _execute;
+        private readonly Func<T, bool> _canExecute;
+
+        /// <summary>
+        /// Constructor not using canExecute.
+        /// </summary>
+        /// <param name="execute"></param>
+        public Command(Action<T> execute) : this(execute, null) { }
+
+        /// <summary>
+        /// Constructor using both execute and canExecute.
+        /// </summary>
+        /// <param name="execute"></param>
+        /// <param name="canExecute"></param>
+        public Command(Action<T> execute, Func<T, bool> canExecute)
+        {
+            _execute = execute;
+            _canExecute = canExecute;
+        }
+
+        /// <summary>
+        /// This method is called from XAML to evaluate if the command can be executed.
+        /// </summary>
+        /// <param name="parameter"></param>
+        /// <returns></returns>
+        public bool CanExecute(object parameter)
+        {
+            return _canExecute == null || _canExecute((T)parameter);
+        }
+
+        /// <summary>
+        /// This method is called from XAML to execute the command.
+        /// </summary>
+        /// <param name="parameter"></param>
+        public void Execute(object parameter)
+        {
+            _execute((T)parameter);
+        }
+
+        /// <summary>
+        /// This method allow us to force the execution of CanExecute method to reevaluate execution.
+        /// </summary>
+        public void RaiseCanExecuteChanged()
+        {
+            CanExecuteChanged?.Invoke(this, new EventArgs());
+        }
+
+        /// <summary>
+        /// This event notify XAML controls using the command to reevaluate the CanExecute of it.
+        /// </summary>
+        public event EventHandler CanExecuteChanged;
     }
 }
--- MediktapAdmin-master/Views/MainWindows/ViewModel/MainWindowViewModel.cs
+++ MediktapAdmin-master/Views/MainWindows/ViewModel/MainWindowViewModel.cs
@@ -2,6 +2,8 @@
 using MediktapAdmin.Templates;
 using MediktapAdmin.ViewModels.Base;
 using MediktapAdmin.Views.MedikTappMenus;
+using System.Windows;
+using System.Windows.Controls;
 using System.Windows.Input;
 
 namespace MediktapAdmin.Views.MainWindows
@@ -11,13 +13,22 @@ namespace MediktapAdmin.Views.MainWindows
 
         public MainWindowViewModel(NavigationService navigationService) : base(navigationService)
         {
-            LoginCmd = new Command(() => NavigationService.GoTo<MedikTappMenu>());
+            LoginCmd = new Command<PasswordBox>(Login);
 
             helpCmd = new Command(() => NavigationService.GoTo<HelpWindow.HelpWindow>());
         }
 
         public ICommand helpCmd { get; set; }
         public ICommand LoginCmd { get; }
+        public string Username { get; set; }
+        public PasswordBox Password { get; set; }
 
+        private void Login(PasswordBox passwordBox)
+        {
+            if (Username == "admin" && passwordBox.Password == "1234")
+                NavigationService.GoTo<MedikTappMenu>();
+            else
+                MessageBox.Show("You entered a wrong password, please try again", "Wrong password");
+        }
     }
 }
